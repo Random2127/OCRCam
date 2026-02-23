4e8ecdee2636f86f1a2f6ad2cce605bbab15fa31
Random2127 <vmedina1978@gmail.com>
2026-02-23 20:09:32 +0100

    gitignore issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,16 +24,18 @@ android {
         applicationId = "com.example.ocrcam"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = false
+            isShrinkResources = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
